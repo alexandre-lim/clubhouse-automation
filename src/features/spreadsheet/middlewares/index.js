745748a314ac3ce8bfe7087496eb1bb261114118
Alexandre Lim <alexandre.lim@abbeal.com>
2019-11-02 20:54:56 +0100

    refactor(spreadsheet): split files and functions into units for better use

--- src/features/spreadsheet/middlewares/index.js
+++ src/features/spreadsheet/middlewares/index.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import {
-  getArticles,
-  writeArticles,
+  getArticlesRoute,
+  writeArticlesRoute,
 } from './routes';
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.get('/articles', getArticles);
+router.get('/articles', getArticlesRoute);
 
-router.post('/articles', writeArticles);
+router.post('/articles', writeArticlesRoute);
 
 export {
   router as spreadsheetRouter,
