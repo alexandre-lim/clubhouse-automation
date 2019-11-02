49bb71ebee0311ebf47e8a44482a22e2457a304d
Alexandre Lim <alexandre.lim@abbeal.com>
2019-11-02 22:06:58 +0100

    refactor(spreadsheet): use Tests tab for writing tests

--- src/features/spreadsheet/middlewares/routes.js
+++ src/features/spreadsheet/middlewares/routes.js
@@ -2,7 +2,7 @@ import { readSingleRange } from '../read';
 import { writeMultipleRanges } from '../write';
 
 async function getArticlesRoute(req, res) {
-  const tab = 'Articles';
+  const tab = 'Tests';
   const range = 'A3:D5';
   const results = await readSingleRange(tab, range);
   res.json(results);
@@ -13,14 +13,14 @@ async function writeArticlesRoute(req, res) {
     valueInputOption: 'USER_ENTERED',
     data: [
       {
-        range: 'Articles!A3',
+        range: 'Tests!A3',
         majorDimension: 'COLUMNS',
         values: [
           ['']
         ]
       },
       {
-        range: 'Articles!A4:C4',
+        range: 'Tests!A4:C4',
         majorDimension: 'ROWS',
         values: [
           ['Write title', 'Description', 'Url value'],
