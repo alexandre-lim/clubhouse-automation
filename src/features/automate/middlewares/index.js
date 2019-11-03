f94992de234d11841f996b19bcd911304d2b76bc
Alexandre Lim <alexandre.lim@abbeal.com>
2019-11-03 17:06:05 +0100

    feature(automate): add get book data in sheets to create epic and associated stories [branch ch93]

--- src/features/automate/middlewares/index.js
+++ src/features/automate/middlewares/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   automateArticlesRoute,
+  automateBookRoute,
 } from './routes';
 
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.post('/articles', automateArticlesRoute);
+
+router.post('/book', automateBookRoute);
 
 export {
   router as automateRouter,
